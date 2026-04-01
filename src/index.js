155ab7d5fa10deaeb947c095a2bfda33ced0ce40
Shreyas <shreyaskarade1517@gmail.com>
2026-04-02 01:41:16 +0530

    Utility code disscussion

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ dotenv.config({
    path: "./.env"
 });
 
-connectDB().catch((error) => {
+connectDB()
+.then(() => {
+   app.listen(process.env.PORT || 8000,()=>{ console.log(`Server is running at PORT:${process.env.PORT}`);
+   })
+})
+
+.catch((error) => {
    console.error("Startup failed:", error);
    process.exit(1);
 });
